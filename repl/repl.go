a9a8093a4427b16982a8a1dc55bb8fb403f29bf1
Niko Bentley <niko@maplestorm.ca>
2024-03-29 22:19:08 -0300

    updated repl to add parser and an error message

--- repl/repl.go
+++ repl/repl.go
@@ -3,12 +3,17 @@ package repl
 import (
 	"bufio"
 	"cottagepie/lexer"
-	"cottagepie/token"
+	"cottagepie/parser"
 	"fmt"
 	"io"
 )
 
 const PROMPT = ">> "
+const ERROR_MESSAGE = `
+#========================#
+#      ERRORS FOUND      #
+#========================#
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,9 +27,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, ERROR_MESSAGE)
+	io.WriteString(out, "\nEncountered errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
